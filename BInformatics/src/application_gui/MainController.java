3678228a74d88d6ffd465ea7b9fd0fa38faf86fa
USER <USER@PC>
2018-10-25 19:40:15 +0900

    Added all parameters in javafx Scene and in controllerclass

--- BInformatics/src/application_gui/MainController.java
+++ BInformatics/src/application_gui/MainController.java
@@ -12,8 +12,7 @@ import javafx.stage.Stage;
 
 public class MainController {
     final FileChooser fileChooser = new FileChooser();
-
-	
+    //A stage in which files can be searched after on
 	private Stage fileStage = new Stage();
 	@FXML
 	Button rpathbtn = new Button();
@@ -40,6 +39,19 @@ public class MainController {
 	TextField phenopath = new TextField();
 	String phenoPathText = new String();
 
+	@FXML
+	//textfields for pvalue data and metasoft data, I think some must be parsed into Integer except mvalue_method
+	TextField min_gene = new TextField();
+	@FXML
+	TextField mvalue_threshold = new TextField();
+	
+	//Metasoft input
+	@FXML TextField mvalue_method = new TextField();
+	@FXML TextField mcmc_sample = new TextField();
+	@FXML TextField seed = new TextField();
+	@FXML TextField mvalue_p_thres = new TextField();
+	@FXML TextField mvalue_prior_sigma = new TextField();
+	@FXML TextField mvalue_prior_beta = new TextField();
 	@FXML
 	Button runbtn = new Button();
 	
@@ -112,6 +124,22 @@ public class MainController {
 	
 	@FXML
 	private void runScript() {
+		//Gettext from these
+		TextField rpath = new TextField();
+		TextField javapath = new TextField();
+		TextField snppath = new TextField();
+		TextField phenopath = new TextField();
+		//use p-value from the standard ttest using the t_test_static in the NICE package. -pvalue = ./test/p_ttest.txt
+		//out is default -out = ./test/ but maybe this can be changed to give an output in a javafx scene
+		TextField min_gene = new TextField();
+		TextField mvalue_threshold = new TextField();
+		
+		TextField mvalue_method = new TextField();
+		TextField mcmc_sample = new TextField();
+		TextField seed = new TextField();
+		TextField mvalue_p_thres = new TextField();
+		TextField mvalue_prior_sigma = new TextField();
+		TextField mvalue_prior_beta = new TextField();
 		//Buildconfig
 		/*
 		try {
